3bf16c483e952c3a32faf035e2539a97f280a400
Maciej Król <maciekgplay@gmail.com>
2023-06-01 21:04:45 +0200

    Basic UI setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,28 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body className={`overflow-clip ${inter.className}`}>
         <Titlebar/>
-        <div className="h-screen overflow-clip">
-          <div className="h-screen overflow-auto border-slate-900 pb-8">
-            {children}
-          </div>
+          <div className="flex flex-col mt-10 gap-24 h-screen overflow-auto">
+            <div className="flex gap-32 px-20 grow">
+                <aside className="flex flex-col gap-6 grow max-w-[400px]">
+                  <div className="flex gap-3">
+                    <button className="bg-[#161617] rounded-2xl min-h-[40px] grow">Home</button>
+                    <button className="bg-[#161617] rounded-2xl min-h-[40px] grow">Settings</button>
+                  </div>
+                  <div className="flex flex-col grow p-8 bg-[#161617] rounded-2xl">
+                    <div className="flex justify-between">
+                      <h1>Playlists</h1>
+                      <button>+</button>
+                    </div>
+                    <div className="flex flex-col gap-5">
+                      <p>Playlists go here...</p>
+                    </div>
+                  </div>
+                </aside>
+                <main className="flex flex-col grow p-8 bg-[#161617] rounded-2xl">
+                  {children}
+                </main>
+            </div>
+          <div id="audio-player-placeholder" className="w-full grow max-h-[200px] bg-[#161617]">Lorem ipsum dolor sit amet.</div>
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex flex-col items-center justify-between p-24">
-			<h1 className="text-xl font-bold leading-tight">
-				Welcome to Apollo
-			</h1>
-		</main>
+		<div>
+			<h1 className="text-5xl">Welcome to Apollo</h1>
+			<p className="text-2xl mt-5">This is a placeholder</p>
+		</div>
 	);
 }
