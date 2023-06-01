eee9e36934dfacae412784c28601162637a0cdea
Maciej Król <maciekgplay@gmail.com>
2023-06-01 21:05:02 +0200

    Titlebar adjustment

--- src/components/organisms/titlebar.tsx
+++ src/components/organisms/titlebar.tsx
@@ -7,7 +7,7 @@ const Titlebar: React.FC = () => {
     return (
         <div className="flex justify-between items-center h-10 pr-2">
             <div data-tauri-drag-region className="w-full h-full"/>
-            <div className='flex gap-3'>
+            <div className='flex gap-5'>
                 <button onClick={() => appWindow.minimize()}>
                     <Minus/>
                 </button>
