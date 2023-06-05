b2ad6a2ed46fbde345ed80870186cfc97b698585
Maciej Król <maciekgplay@gmail.com>
2023-06-05 19:48:46 +0200

    Refreshing the app after clearing localStorage

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,7 +4,10 @@ const SettingsPage: React.FC = () => {
     return (
         <div>
             <h1 className="text-5xl">App settings</h1>
-            <button onClick={() => localStorage.clear()}>Wipe localStorage</button>
+            <button onClick={() => {
+                localStorage.clear()
+                window.location.reload()
+            }}>Wipe localStorage</button>
         </div>
     )
 }
