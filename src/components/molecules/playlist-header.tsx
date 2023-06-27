0ea52ff31f20571252b77077181ec3922e034489
Maciej Król <maciekgplay@gmail.com>
2023-06-27 19:21:59 +0200

    Removal of an unused context value

--- src/components/molecules/playlist-header.tsx
+++ src/components/molecules/playlist-header.tsx
@@ -10,7 +10,7 @@ interface PlaylistHeaderProps {
 }
 
 const PlaylistHeader: React.FC<PlaylistHeaderProps> = (
-    {targetPlaylist, playlistsArray, setPlaylistsArray}
+    {targetPlaylist, setPlaylistsArray}
 ) => {
     return (
         <header className="flex gap-7">
