0550722615c73a58996d2e4c3699a58f28130489
Maciej Król <maciekgplay@gmail.com>
2023-06-27 19:19:45 +0200

    Audio bar update
    Audio bar now has its content rendered only after a song has been selected

--- src/components/molecules/audio-bar.tsx
+++ src/components/molecules/audio-bar.tsx
@@ -9,23 +9,27 @@ const AudioBar: React.FC = () => {
     const {currentSong} = useContext(AppAudioContext) as AppAudioContextValues
 
     return (
-        <div className="grid grid-cols-3 p-8 items-center bg-menus-background">
-            <div/>
+        <div className="grid grid-cols-3 min-h-[156px] mt-2 p-8 items-center bg-menus-background">
+            {currentSong &&
+                <>
+                    <div/>
 
-            <div className="flex flex-col gap-3 items-center">
-                <PlaybackController/>
-                <SeekBar/>
-                <p 
-                    className="text-sm text-center max-w-full max-h-[28px] text-ellipsis overflow-hidden whitespace-nowrap"
-                    title={currentSong?.title}
-                >
-                    {currentSong?.title}
-                </p>
-            </div>
+                    <div className="flex flex-col gap-3 items-center">
+                        <PlaybackController/>
+                        <SeekBar/>
+                        <p 
+                            className="text-sm text-center max-w-full max-h-[28px] text-ellipsis overflow-hidden whitespace-nowrap"
+                            title={currentSong?.title}
+                        >
+                            {currentSong?.title}
+                        </p>
+                    </div>
 
-            <div className="flex justify-end">
-                <VolumeController/>
-            </div>
+                    <div className="flex justify-end">
+                        <VolumeController/>
+                    </div>
+                </>
+            }
         </div>
     )
 }
