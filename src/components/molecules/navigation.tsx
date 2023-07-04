71075b38ba7572c49fc544a81e6c933d77290f7a
Maciej Król <maciekgplay@gmail.com>
2023-07-04 13:21:15 +0200

    Improve navigation panel

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 const Navigation: React.FC = () => {
     return (
         <div className="flex flex-col gap-4 rounded-2xl text-xl bg-menus-background p-4">
-            <button className="flex items-center gap-3 text-left">
-                <Home/> <Link href="/">Home</Link>
-            </button>
-            <button className="flex items-center gap-3 text-left">
-                <Settings/> <Link href="/settings">Settings</Link>
-            </button>
+            <Link className="flex items-center gap-3 text-left" href="/">
+                <Home/> Home
+            </Link>
+            <Link className="flex items-center gap-3 text-left" href="/settings">
+                <Settings/> Settings
+            </Link>
         </div>
     )
 }
