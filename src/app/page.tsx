efcc441efe6369e7070c0903db229458cc1defbd
Maciej Król <maciekgplay@gmail.com>
2023-07-13 13:33:02 +0200

    Simplify the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { AppAudioContext } from "@/contexts/app-audio-context";
-import { AppAudioContextValues } from "@/ts/interfaces";
-import { useContext } from "react";
-
-export default function Home() {
-
-	const { playlistsArray } = useContext(AppAudioContext) as AppAudioContextValues
-
+const Home: React.FC = () => {
 	return (
 		<div>
-			<h1 className="text-5xl">Welcome to Apollo</h1>
-			<p className="text-2xl mt-5">This is a placeholder</p>
-			<p>This component is using the AppAudioContext</p>
-			<p>Current playlist array looks like so:</p>
-			<p>{JSON.stringify(playlistsArray)}</p>
+			<h1 className="text-5xl font-semibold">Welcome to Apollo</h1>
+			<p className="my-4 text-lg">Create a new playlist using the plus button on the sidebar</p>
+			<p className="text-lg">Recently played playlists will be displayed on this page Soon&#8482;</p>
 		</div>
 	);
 }
+
+export default Home
